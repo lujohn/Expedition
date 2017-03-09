805cef74bd464352d9b9fe848063a9e43f295ff1
John Lu <luj1991@gmail.com>
2017-03-08 23:16:32 -0500

    add mapGraphService.js and playerServiceTest.js files

---
+++ tests/unit/playerServiceTest.js
@@ -0,0 +1,18 @@
+describe('PlayerService Tests', function () {
+	beforeEach(module('expeditionApp'));
+
+	var PlayerService, LandFactory;
+
+	beforeEach(inject(function(_PlayerService_, _LandFactory_) {
+		PlayerService = _PlayerService_;
+		LandFactory = _LandFactory_;
+	}));
+
+	it('should create red settlement', function () {
+
+	});
+
+	it('should create blue road', function () {
+
+	});
+});
